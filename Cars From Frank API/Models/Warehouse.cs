3e7e7126634730b5a8d8aaa768584b461d3b7d59
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-08 22:04:15 +0100

    Add Warehouse.cs model

---
+++ Cars From Frank API/Models/Warehouse.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Cars_From_Frank_API.Models
+{
+    public class Warehouse
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public int? Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("location")]
+        public Location WarehouseLocation { get; set; }
+        [BsonElement("cars")]
+        public List<Car> CarsInWarehouse { get; set; }
+    }
+}
