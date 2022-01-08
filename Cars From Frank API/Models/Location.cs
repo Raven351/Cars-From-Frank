44927e582c0180a18e6e54a6bea8159114694f8d
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-08 22:10:56 +0100

    Add location model

---
+++ Cars From Frank API/Models/Location.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Cars_From_Frank_API.Models
+{
+    public class Location
+    {
+        [BsonElement("lat")]
+        public double Latitude { get; set; }
+        [BsonElement("long")]
+        public double Longitude { get; set; }
+    }
+}
