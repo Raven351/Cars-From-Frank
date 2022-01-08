e50ef9b95a8b3e6c44d65d2d1f5cbef927e02b0c
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-08 22:23:47 +0100

    Add Cars.cs model

---
+++ Cars From Frank API/Models/Cars.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Cars_From_Frank_API.Models
+{
+    public class Cars
+    {
+        [BsonElement("location")]
+        public string Location { get; set; }
+
+        [BsonElement("vehicles")]
+        public List<Vehicle> Vehicles { get; set; }
+    }
+}
