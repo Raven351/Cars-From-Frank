ec69d03973b720fef6934a547833a077a1bf7c8d
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-09 10:03:54 +0100

    Add WarehousesController.cs

---
+++ Cars From Frank API/Controllers/WarehousesController.cs
@@ -0,0 +1,33 @@
+﻿using Cars_From_Frank_API.Models;
+using Cars_From_Frank_API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars_From_Frank_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WarehousesController : ControllerBase
+    {
+        private readonly WarehousesService _warehousesService;
+
+        public WarehousesController(WarehousesService warehousesService) =>
+            _warehousesService = warehousesService;
+
+        [HttpGet]
+        public async Task<List<Warehouse>> Get() =>
+            await _warehousesService.GetAsync();
+
+        [HttpGet("{id:length(16)}")]
+        public async Task<ActionResult<Warehouse>> Get(string id)
+        {
+            var warehouse = await _warehousesService.GetAsync(id);
+
+            if (warehouse is null)
+            {
+                return NotFound();
+            }
+
+            return warehouse;
+        }
+    }
+}
