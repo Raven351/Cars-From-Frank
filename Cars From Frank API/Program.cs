0ea6b09e385cbb21740d2e7d5fa60fab1460578a
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-09 09:42:04 +0100

    Add WarehousesService.cs

--- Cars From Frank API/Program.cs
+++ Cars From Frank API/Program.cs
@@ -1,10 +1,13 @@
 using Cars_From_Frank_API.Models;
+using Cars_From_Frank_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<CarsFromFrankDatabaseSettings>(
     builder.Configuration.GetSection("CarsFromFrankDatabase"));
+
+builder.Services.AddSingleton<WarehousesService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Cars From Frank API/Services/WarehousesService.cs
@@ -0,0 +1,26 @@
+﻿using Cars_From_Frank_API.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Cars_From_Frank_API.Services
+{
+    public class WarehousesService
+    {
+        private readonly IMongoCollection<Warehouse> _warehousesCollection;
+
+        public WarehousesService(IOptions<CarsFromFrankDatabaseSettings> carsFromFrankDatabaseOptions)
+        {
+            var mongoClient = new MongoClient(carsFromFrankDatabaseOptions.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(carsFromFrankDatabaseOptions.Value.DatabaseName);
+
+            _warehousesCollection = mongoDatabase.GetCollection<Warehouse>(carsFromFrankDatabaseOptions.Value.CollectionName);
+        }
+
+        public async Task<List<Warehouse>> GetAsync() =>
+            await _warehousesCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Warehouse?> GetAsync(string id) =>
+            await _warehousesCollection.Find(id).FirstOrDefaultAsync();
+    }
+}
